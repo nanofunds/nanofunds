fd8ab1c410e1f75ee5e5078f098ab27ef1da5d34
Eddie Rozum <eddie.rozum@orderwithme.com>
2015-10-24 20:12:53 -0700

    add basic auth

--- nanofunds/Controllers/HomeController.cs
+++ nanofunds/Controllers/HomeController.cs
@@ -8,8 +8,13 @@ namespace nanofunds.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string merchant_id, string employee_id, string client_id, string code)
         {
+
+            if (string.IsNullOrEmpty(code))
+            {
+                this.Response.Redirect("https://sandbox.dev.clover.com/oauth/authorize?client_id=4DCJSK51BSG52&redirect_url=http://localhost:2275/");
+            }
             return this.View();
         }
     }
