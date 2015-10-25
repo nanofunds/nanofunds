790ebd2b7a600528b9ece10e674dce9a8bf72d6e
Mario Tacke <mario@orderwithme.com>
2015-10-24 22:39:16 -0700

    Merge

--- nanofunds/Models/Account.cs
+++ nanofunds/Models/Account.cs
@@ -3,6 +3,8 @@
     using System;
     using System.Collections.Generic;
 
+    using Enums;
+
     public class Account
     {
         public Account()
@@ -13,6 +15,8 @@
         public decimal Balance { get; set; }
 
         public Guid Id { get; set; }
+
+        public Merchant Merchant { get; set; }
 
         public List<Transaction> Transactions { get; set; }
     }
---
+++ nanofunds/Models/Repayment.cs
@@ -0,0 +1,26 @@
+﻿namespace nanofunds.Models
+{
+    using System;
+
+    public class Repayment
+    {
+        public Guid Id { get; set; } 
+
+        public Account FromAccount { get; set; }
+
+        public Account ToAccount { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime ChargeOnDate { get; set; }
+
+        public Repayment(Account from, Account to, decimal amount, DateTime chargeOnDate)
+        {
+            Id = Guid.NewGuid();
+            FromAccount = from;
+            ToAccount = to;
+            Amount = amount;
+            ChargeOnDate = chargeOnDate;
+        }
+    }
+}
