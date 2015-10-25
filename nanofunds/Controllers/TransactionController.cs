d6cb2551f9f5199a9caae79fa49e97aaf9175c32
Mario Tacke <mario@orderwithme.com>
2015-10-25 04:14:33 -0700

    Add transaction route

---
+++ nanofunds/Controllers/TransactionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace nanofunds.Controllers
+{
+    using Models;
+    using Models.Enums;
+
+    public class TransactionController : ApiController
+    {
+        public object Get(string id)
+        {
+            var db = new nanofunds();
+
+            var account =
+                db.Ledgers.SingleOrDefault(x => x.Name == "main").Accounts.SingleOrDefault(x => x.Merchant.SourceId == id);
+
+            return account.Transactions.OrderByDescending(x=>x.Timestamp).Select(x => new
+            {
+                Date = x.Timestamp.Date,
+                Kind = Enum.GetName(typeof(TTransactionKind), x.Kind),
+                Type = Enum.GetName(typeof(TTransaction), x.Type),
+                Amount = x.Amount
+            });
+        }
+    }
+}
